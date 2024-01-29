319271d6456d394e078de620b7498ea6d0595bad
barjakoub <oujakdev.rep@gmail.com>
2024-01-30 01:26:43 +0700

    accomplished: main page 100% complete -> responsive any breakpoints

--- src/routers/home.route.js
+++ src/routers/home.route.js
@@ -6,7 +6,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.info(req.query);
     res.render(`pages/index`, { pageTitle: `Mini - ATS` });
 }).get('/component', (req, res) => {
     res.render(`example`, { layout: `layouts/example.layout.ejs`, pageTitle: `Example Component` });
